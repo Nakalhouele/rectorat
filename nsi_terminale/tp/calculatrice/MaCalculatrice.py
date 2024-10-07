813210c7731c65395d4b9f03b98b1384495716ae
Koné <romeo.kone@outlook.com>
2024-10-07 10:56:58 -0400

    Encapsulation dans la clase mère Calculatrice

--- nsi_terminale/tp/calculatrice/MaCalculatrice.py
+++ nsi_terminale/tp/calculatrice/MaCalculatrice.py
@@ -12,7 +12,12 @@ class Addition():
     
     def calcule(self, a, b):
         return a + b
-
+    
+    def recuperer_saisie(self) -> tuple[int, int]: 
+        print("Vous souhaitez faire une Addition")
+        a = int(input("Saisir le premier nombre : "))
+        b = int(input("Saisir le deuxieme nombre : "))
+        return a, b
 
 class Soustraction():
     
@@ -50,40 +55,55 @@ class RacineCarre():
         return nombre ** 0.5
 
 
-# MAIN
-addition = Addition()
-soustraction = Soustraction()
-multiplication = Multiplication()
-division = Division()
-racine_carre = RacineCarre()
+class Calculatrice():
+    
+    def __init__(self, nom: str) -> None:
+        self.nom = nom
+        print(f'Bienvenue sur {self.nom}')
+        
+    def execute(self) -> None: 
+        addition = Addition()
+        soustraction = Soustraction()
+        multiplication = Multiplication()
+        division = Division()
+        racine_carre = RacineCarre()
 
-volonte = input("Que veux tu faire : ")
-if volonte == "0":
-    print("Vous souhaitez faire une Addition")
-    a = int(input("Saisir le premier nombre : "))
-    b = int(input("Saisir le deuxieme nombre : "))
-    resultat = addition.calcule(a, b)
-    print(resultat)
-elif volonte == "1":
-    print("Vous souhaitez faire une Soustraction")
-    a = int(input("Saisir le premier nombre : "))
-    b = int(input("Saisir le deuxieme nombre : "))
-    resultat = soustraction.calcule(a, b)
-    print(resultat)
-elif volonte == "2":
-    print("Vous souhaitez faire une Multiplication")
-    a = int(input("Saisir le premier nombre : "))
-    b = int(input("Saisir le deuxieme nombre : "))
-    resultat = multiplication.calcule(a, b)
-    print(resultat)
-elif volonte == "3":
-    print("Vous souhaitez faire une Division")
-    a = int(input("Saisir le premier nombre : "))
-    b = int(input("Saisir le deuxieme nombre : "))
-    resultat = division.calcule(a, b)
-    print(resultat)
-elif volonte == "4":
-    print("Vous souhaitez faire une Racine Carre")
-    a = int(input("Saisir le nombre : "))
-    resultat = racine_carre.calcule(a)
-    print(resultat)
+        volonte = input("Que veux tu faire : ")
+        if volonte == "0":
+            # print("Vous souhaitez faire une Addition")
+            # a = int(input("Saisir le premier nombre : "))
+            # b = int(input("Saisir le deuxieme nombre : "))
+            a, b = addition.recuperer_saisie()
+            resultat = addition.calcule(a, b)
+            print(resultat)
+        elif volonte == "1":
+            print("Vous souhaitez faire une Soustraction")
+            a = int(input("Saisir le premier nombre : "))
+            b = int(input("Saisir le deuxieme nombre : "))
+            resultat = soustraction.calcule(a, b)
+            print(resultat)
+        elif volonte == "2":
+            print("Vous souhaitez faire une Multiplication")
+            a = int(input("Saisir le premier nombre : "))
+            b = int(input("Saisir le deuxieme nombre : "))
+            resultat = multiplication.calcule(a, b)
+            print(resultat)
+        elif volonte == "3":
+            print("Vous souhaitez faire une Division")
+            a = int(input("Saisir le premier nombre : "))
+            b = int(input("Saisir le deuxieme nombre : "))
+            resultat = division.calcule(a, b)
+            print(resultat)
+        elif volonte == "4":
+            print("Vous souhaitez faire une Racine Carre")
+            a = int(input("Saisir le nombre : "))
+            resultat = racine_carre.calcule(a)
+            print(resultat)
+
+
+# MAIN
+
+ma_calculatrice = Calculatrice("Martinique Instruments")
+ma_calculatrice.execute()
+
+
